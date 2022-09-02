e9382d4f2705e8c26575644e26fca56cb51239c2
Mark Delchev <markdelchev@gmail.com>
2022-09-02 21:21:22 +0300

    odd_even.js

---
+++ Programming_Basics/Conditional_Statements_Lab/odd_even.js
@@ -0,0 +1,13 @@
+function even(input)
+{
+    let num = input[0];
+    if (num % 2 === 0)
+    {
+        console.log("even");
+    }
+    else
+    {
+        console.log("odd");
+    }
+}
+even(["4"])
