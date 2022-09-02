76b78f2be3d483b58860684376c0298c1b053a6e
Mark Delchev <markdelchev@gmail.com>
2022-09-02 21:17:26 +0300

    bigger_num.js

---
+++ Programming_Basics/Conditional_Statements_Lab/bigger_num.js
@@ -0,0 +1,14 @@
+function bigger(input)
+{
+    let num1 = input[0];
+    let num2 = input[1];
+    if (num1 > num2)
+    {
+        console.log(num1)
+    }
+    else
+    {
+        console.log(num2)
+    }
+}
+bigger(["5", "3"])
