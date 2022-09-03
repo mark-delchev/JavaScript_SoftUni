49bf49740a7b444644186a7b69115dde3ca4fce4
Mark Delchev <markdelchev@gmail.com>
2022-09-03 21:27:58 +0300

    bonus_score.js

---
+++ Programming_Basics/Conditional_Statements_Exercise/bonus_score.js
@@ -0,0 +1,29 @@
+function bonus(input)
+{
+    let score = input[0];
+    let bonus = 0;
+    if (score <= 100)
+    {
+        bonus += 5;
+    }
+    else if (score > 100 && score <= 1000)
+    {
+        bonus = score * 0.2;
+    }
+    else
+    {
+        bonus = score * 0.1;
+    }
+    if (score % 2 === 0)
+    {
+        bonus += 1;
+    }
+    else if (score % 10 === 5)
+    {
+        bonus += 2;
+    }
+    let final_score = +score + +bonus;
+    console.log(bonus);
+    console.log(final_score);
+}
+bonus(["20"])
