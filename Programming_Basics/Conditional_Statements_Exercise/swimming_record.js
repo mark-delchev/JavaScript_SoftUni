3b9519241c7684c78a074f386cd6c8c0c08db5a5
Mark Delchev <markdelchev@gmail.com>
2022-09-06 12:21:45 +0300

    record.js fix

--- Programming_Basics/Conditional_Statements_Exercise/swimming_record.js
+++ Programming_Basics/Conditional_Statements_Exercise/swimming_record.js
@@ -4,15 +4,16 @@ function record(input)
     let meters = Number(input[1]);
     let seconds = Number(input[2]);
     let time_needed = seconds * meters;
-    let slowing_down = Math.floor(seconds / 15);
+    let slowing_down = Math.floor(meters / 15);
     let time_needed_final = time_needed + slowing_down * 12.5;
-    if (time_needed_final > record)
+    if (time_needed_final >= record)
     {
         let seconds_left = time_needed_final - record;
-        console.log(`No, he failed! He was ${seconds_left} seconds slower.`);
+        console.log(`No, he failed! He was ${seconds_left.toFixed(2)} seconds slower.`);
     }
     else
     {
-        console.log(`Yes, he succeeded! The new world record is ${time_needed_final} seconds.`);
+        console.log(`Yes, he succeeded! The new world record is ${time_needed_final.toFixed(2)} seconds.`);
     }
 }
+record(["10464", "1500", "20"]);
