3a50e4fe1622c36bc9b366cd5dc3123a8dbd3878
Mark Delchev <markdelchev@gmail.com>
2022-09-27 22:36:03 +0300

    cinema_ticket.js

---
+++ Programming_Basics/Conditional_Statements_Advanced_Lab/cinema_ticket.js
@@ -0,0 +1,17 @@
+function cinema(input)
+{
+    let day = input[0];
+
+    if (day == "Monday" || day == "Tuesday" || day == "Friday")
+    {
+        console.log(12);
+    }
+    else if (day == "Saturday" || day == "Sunday")
+    {
+        console.log(16);
+    }
+    else
+    {
+        console.log(14);
+    }
+}
